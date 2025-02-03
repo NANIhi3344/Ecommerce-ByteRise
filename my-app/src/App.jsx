58f32deedea3257fc6769f481682de7405dc3654
NANI <@github.comgit config --global user.email>
2025-02-03 16:01:01 +0530

    good

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import { Login } from './Component/Login'
 import { Signup } from './Component/Signup'
 import { Home } from './page/Home'
+import { Productform } from './Component/Productform'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Route path="/Home" element={<Home/>} />
       <Route path="/Login" element={<Login/>} />
       <Route path="/Signup" element={<Signup/>} />
+      <Route path="/Productform" element={<Productform/>} />
     </Routes>
     </>
   )
