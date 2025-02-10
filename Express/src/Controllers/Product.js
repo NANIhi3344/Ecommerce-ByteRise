7f712585d41fcea37b29be635e246c7889d3818f
NANI <@github.comgit config --global user.email>
2025-02-10 15:58:41 +0530

    good

--- Express/src/Controllers/Product.js
+++ Express/src/Controllers/Product.js
@@ -1,44 +1,55 @@
 const {Router} = require('express');
-const { Productcard } = require('../Model/Productmodel');
-const { productupload } = require('../../multer');
-
+const Productmodel = require('../Model/ProductModel');
 const productrouter = Router();
 
-productrouter.get('/',(req,res)=>{
-    res.send('Productcard')
-})
-
-productrouter.post('/',productupload.array('files'),async(req,res)=>{
-    const {name,description,price,ratings,email,category,stock,numOfReviews,reviews}=req.body;
-    const images = req.files.map(file=>file.path);
+ productrouter.get("/get-product",async (req,res)=>{
     try{
-        const seller = await Productmodel.findOne({email:email});
-        if(!seller){
-            return res.status(400).json({message:"Seller not found"});
-        }
+        const productfind=await Productmodel.find();
+        const productimages = productfind.map((product)=>{
+            return{
+                name:product.name,
+                description:product.description,
+                category:product.category,
+                tags:product.tags,
+                price:product.price,
+                email:product.email,
+                stock:product.stock,
+                images:product.images,
 
-        if(images.length==0){
-            return res.status(400).json({message:"Please upload atleast one image"});
-        }
-        await Productmodel.create({ 
-            name:name,
-            description:description,
-            price:price,
-            ratings:ratings,
-            email:email,
-            images:images,
-            category:category,
-            stock:stock,
-            numOfReviews:numOfReviews,
-            reviews:reviews,
+            }
         });
-         
-    } catch (error) {
-        res.status(500).json({ message: "Internal Server Error", error: error.message });
+        res.status(200).json({products:products});
     }
-    
+    catch(err){
+        console.log(err)
+    }
+    })
+    productrouter.post("/post-product", productupload.array('files'), async (req, res) => {
+        const { name, price, description, category, tags, stock, email } = req.body;
+        const images = req.files.map(file => file.path);
+        try{
+            const seller = await Productmodel.findOne({email:email});
+            if(!seller){
+                return res.status(400).json({message:"Seller not found"});
+            }
+            if(images.length === 0){
+                return res.status(400).json({message:"Please upload atleast one images"});
+            }
+            await Productmodel.create({
+                name: name,
+                price:price,
+                description: description,
+                category: category,
+                tags:tags,
+                stock: stock,
+                email:email,
+                images: images,
+            });
+            res.status(200).json({message:"Product created successfully",product:newProduct});
+        }
+        catch(err){
+            console.log(err);
+        }
+    })
 
-})
-
-
-module.exports = productrouter;
+    module.exports = productrouter;
